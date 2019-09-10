004357ae5fbdfdc10b8177c9f13ca3c017b8f367
souza.leandror@gmail.com <souza.leandror@gmail.com>
2019-09-10 20:12:45 -0300

    Updated 20%

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
+import 'package:carros/widgets/app_button.dart';
+import 'package:carros/widgets/app_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,6 +9,15 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _tLogin = TextEditingController();
+  final _tPassword = TextEditingController();
+
+  //final _tLogin = TextEditingController(text: "teste2");
+  //final _tPassword = TextEditingController(text: "teste2");
+
+  final _formKey = GlobalKey<FormState>();
+  final _focusSenha = FocusNode();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +30,72 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   _body() {
-    return Container(
-      color: Colors.white,
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            AppText("Login", "Digite o Login",
+                controller: _tLogin,
+                validator: _validatorLogin,
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                nextFocus: _focusSenha),
+            SizedBox(
+              height: 10,
+            ),
+            AppText(
+              "Senha",
+              "Digite a Senha",
+              password: true,
+              controller: _tPassword,
+              validator: _validatorPassword,
+              keyboardType: TextInputType.number,
+              focusNode: _focusSenha,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            AppButton(
+              text: "Login",
+              onPressed: _onClickLogin,
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  String _validatorLogin(String text) {
+    if (text.isEmpty) {
+      return "Digite o login";
+    } else {
+      return null;
+    }
+  }
+
+  String _validatorPassword(String text) {
+    if (text.isEmpty) {
+      return "Digite o password";
+    }
+    if (text.length < 3) {
+      return "O minimo 3 caracteres";
+    } else {
+      return null;
+    }
+  }
+
+  void _onClickLogin() {
+    bool formOk = _formKey.currentState.validate();
+
+    if (!formOk) {
+      return;
+    }
+
+    String login = _tLogin.text;
+    String senha = _tPassword.text;
+
+    print("Login = ${login} e senha = ${senha}");
   }
 }
---
+++ lib/widgets/app_text.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  String text;
+  String hintText;
+  bool password;
+  TextEditingController controller;
+  FormFieldValidator<String> validator;
+  TextInputType keyboardType;
+  TextInputAction textInputAction;
+  FocusNode focusNode;
+  FocusNode nextFocus;
+
+  AppText(this.text, this.hintText,
+      {this.password = false,
+      this.controller,
+      this.validator,
+      this.keyboardType,
+      this.textInputAction,
+      this.focusNode,
+      this.nextFocus});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: password,
+      validator: validator,
+      keyboardType: keyboardType,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      onFieldSubmitted: (String text) {
+        if (nextFocus != null) {
+          FocusScope.of(context).requestFocus(nextFocus);
+        }
+      },
+      style: TextStyle(
+        fontSize: 16,
+      ),
+      decoration: InputDecoration(
+        labelText: text,
+        hintText: hintText,
+        labelStyle: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
